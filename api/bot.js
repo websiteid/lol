eb8588e8619a0e26ffbecab7a6568804955f4053
websiteid <junimatalgano@gmail.com>
2026-03-13 08:17:00 +0700

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -29,7 +29,7 @@ app.post('/api/bot', async (req, res) => {
 
 app.get('/set-webhook', async (req, res) => {
   try {
-    const vercelUrl = 'https://script-bot-teleram-vercel.vercel.app';
+    const vercelUrl = 'https://nnn-tau-jet.vercel.app';
     const webhookUrl = `${vercelUrl}/api/bot`;
     
     await bot.deleteWebHook();
